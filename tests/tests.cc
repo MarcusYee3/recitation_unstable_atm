7c26c887316fd9c43a39c4741ff771dec70c58b2
MarcusYee3 <dragrosaur@gmail.com>
2026-02-27 21:20:04 +0000

    recommit

--- tests/tests.cc
+++ tests/tests.cc
@@ -37,7 +37,6 @@ bool CompareFiles(const std::string& p1, const std::string& p2) {
 TEST_CASE("Example: Create a new account", "[ex-1]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
-
   auto accounts = atm.GetAccounts();
   REQUIRE(accounts.contains({12345678, 1234}));
   REQUIRE(accounts.size() == 1);
@@ -49,6 +48,7 @@ TEST_CASE("Example: Create a new account", "[ex-1]") {
   auto transactions = atm.GetTransactions();
   REQUIRE(transactions.contains({12345678, 1234}));
   REQUIRE(transactions.size() == 1);
+
   std::vector<std::string> empty;
   REQUIRE(transactions[{12345678, 1234}] == empty);
 
@@ -58,15 +58,16 @@ TEST_CASE("Example: Create a new account", "[ex-1]") {
 
 TEST_CASE("Example: Simple widthdraw", "[ex-2]") {
   Atm atm;
+  Atm dummy;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
   atm.RegisterAccount(22222222, 2222, "Dummy", 0);
-
   REQUIRE_THROWS_AS(atm.WithdrawCash(22222222, 2222, -1),
                     std::invalid_argument);
   REQUIRE_THROWS_AS(atm.WithdrawCash(22222222, 2222, 1), std::runtime_error);
   REQUIRE_THROWS_AS(atm.WithdrawCash(11111111, 1111, 1), std::invalid_argument);
-
   atm.WithdrawCash(12345678, 1234, 20);
+  auto accounts = atm.GetAccounts();
+  Account sam_account = accounts[{12345678, 1234}];
 
   REQUIRE(atm.CheckBalance(12345678, 1234) == 280.30);
   REQUIRE_THROWS_AS(atm.CheckBalance(12345671, 1234), std::invalid_argument);
@@ -76,18 +77,15 @@ TEST_CASE("Example: Simple Deposit", "[ex-3]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
   atm.RegisterAccount(22222222, 2222, "Dummy", 0);
-
   REQUIRE_THROWS_AS(atm.DepositCash(22222222, 2222, -1), std::invalid_argument);
   REQUIRE_THROWS_AS(atm.DepositCash(11111111, 1111, 1), std::invalid_argument);
-
   atm.DepositCash(12345678, 1234, 20);
   auto accounts = atm.GetAccounts();
   Account sam_account = accounts[{12345678, 1234}];
 
   REQUIRE(sam_account.balance == 320.30);
-
   auto transactions = atm.GetTransactions();
-  REQUIRE(transactions[{12345678, 1234}].size() == 1);
+  REQUIRE(transactions.size() == 1);
 }
 
 TEST_CASE("Example: Print Prompt Ledger", "[ex-4]") {
@@ -100,19 +98,8 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-4]") {
       "Deposit - Amount: $40000.00, Updated Balance: $40099.90");
   transactions[{12345678, 1234}].push_back(
       "Deposit - Amount: $32000.00, Updated Balance: $72099.90");
-
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE_THROWS_AS(atm.PrintLedger("./prompt.txt", 11111111, 1239),
                     std::invalid_argument);
-
-  std::string expected_ledger =
-      "Name: Sam Sepiol\n"
-      "Card Number: 12345678\n"
-      "PIN: 1234\n"
-      "----------------------------\n"
-      "Withdrawal - Amount: $200.40, Updated Balance: $99.90\n"
-      "Deposit - Amount: $40000.00, Updated Balance: $40099.90\n"
-      "Deposit - Amount: $32000.00, Updated Balance: $72099.90";
-
-  REQUIRE(CompareFileToString("./prompt.txt", expected_ledger));
+  REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
